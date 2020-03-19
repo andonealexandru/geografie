bb30919729b7da4e6aaf1fdbce563c7798f720e0
Andone Alexandru <andonealexandru19@yahoo.com>
2020-03-19 16:00:04 +0200

    First commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuikit from 'vuikit'
+import VuikitIcons from '@vuikit/icons'
 
-Vue.config.productionTip = false
+import '@vuikit/theme'
+
+Vue.config.productionTip = false;
+Vue.use(Vuikit);
+Vue.use(VuikitIcons);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="full-height uk-height-large uk-background-cover uk-light uk-flex"
+         uk-parallax="bgy: -200"
+         style="background-image: url('https://images.pexels.com/photos/9198/nature-sky-twilight-grass-9198.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500');">
+
+      <h1 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">Headline</h1>
+
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+
+<style>
+  .full-height {
+    height: 100vh;
+  }
+</style>
